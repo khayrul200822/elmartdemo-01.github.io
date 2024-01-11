7cd0f1bfda56c67af8804039ac637a2e393f1ccc
khayrul200822 <khayrulislam200822@gmail.com>
2024-01-12 00:43:44 +0600

    update all files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+//get elements
+const optionMenu = document.querySelectorAll(".currency_option li");
+const option_value = document.querySelector(".value_area");
+const language_option = document.querySelectorAll(".ln_option li")
+const language_value =document.querySelector(".ln_value");
+optionMenu.forEach(item => {
+    item.addEventListener("click" , () => {
+        option_value.textContent = item.textContent;
+    })
+})
+language_option.forEach(ln_item => {
+    ln_item.addEventListener("click" , () => {
+        language_value.textContent = ln_item.textContent;
+    })
+})
