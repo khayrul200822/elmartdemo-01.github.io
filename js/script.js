34929d5ac96cba038160064182375e2c15da66d9
khayrul200822 <khayrulislam200822@gmail.com>
2024-01-13 12:49:56 +0600

    update hero section

--- js/script.js
+++ js/script.js
@@ -14,3 +14,26 @@ language_option.forEach(ln_item => {
         language_value.textContent = ln_item.textContent;
     })
 })
+
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    // loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
