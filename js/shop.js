a0d447c3d4c15137d82192dc40e4d546f76339b0
khayrul200822 <khayrulislam200822@gmail.com>
2024-02-03 00:37:27 +0600

    updated shop page

--- js/shop.js
+++ js/shop.js
@@ -4,7 +4,7 @@ const search_input_area = document.querySelector("#search_input");
 const search_suggestion = document.querySelector(".search_suggestion")
 const close_btn = document.querySelector(".search_close");
 const search_container = document.querySelector(".search_bar");
-const show_button =document.querySelectorAll(".search_icon");
+const show_button = document.querySelectorAll(".search_icon");
 
 
 search_input_area.addEventListener("focus", () => {
@@ -14,9 +14,9 @@ search_input_area.addEventListener("focus", () => {
 //show searh bar 
 show_button.forEach(btn => {
   btn.addEventListener("click", () => {
-  search_container.style.animation = "show_searchbar 1s ease-out forwards alternate";
-  document.body.style.overflow = "hidden";
-})
+    search_container.style.animation = "show_searchbar 1s ease-out forwards alternate";
+    document.body.style.overflow = "hidden";
+  })
 })
 
 //hide search bar
@@ -37,15 +37,15 @@ const close_button = document.querySelector(".close_btn")
 const preloader_area = document.querySelector(".preloader")
 window.addEventListener("load", () => {
   setTimeout(() => {
- preloader_area.style.display = "none"
-  },2000)
+    preloader_area.style.display = "none"
+  }, 2000)
   close_button.addEventListener("click", () => {
     cookies_area.style.animation = "cookies_close 2s ease-in-out  forwards alternate"
-    })
-    cookies_area.style.animation="cookies_open 2s ease-in 2s forwards alternate";
+  })
+  cookies_area.style.animation = "cookies_open 2s ease-in 2s forwards alternate";
   cookies_button.addEventListener("click", () => {
     cookies_area.style.animation = "cookies_close 2s ease-in-out  forwards alternate"
-    })
+  })
 })
 
 //sticky header script
@@ -66,29 +66,72 @@ $(document).ready(() => {
 const optionMenu = document.querySelectorAll(".currency_option li");
 const option_value = document.querySelector(".value_area");
 const language_option = document.querySelectorAll(".ln_option li")
-const language_value =document.querySelector(".ln_value");
+const language_value = document.querySelector(".ln_value");
 optionMenu.forEach(item => {
-    item.addEventListener("click" , () => {
-        option_value.textContent = item.textContent;
-    })
+  item.addEventListener("click", () => {
+    option_value.textContent = item.textContent;
+  })
 })
 language_option.forEach(ln_item => {
-    ln_item.addEventListener("click" , () => {
-        language_value.textContent = ln_item.textContent;
-    })
+  ln_item.addEventListener("click", () => {
+    language_value.textContent = ln_item.textContent;
+  })
 })
- // accordian script
- $(".accordion-content").css("display", "none");
+// accordian script
+$(".accordion-content").css("display", "none");
 
- $(".accordion-title").click(function () {
-    $(".accordion-title").not(this).removeClass("open");
-    $(".accordion-title").not(this).next().slideUp(300);
-    $(this).toggleClass("open");
-    $(this).next().slideToggle(300);
- });
+$(".accordion-title").click(function () {
+  $(".accordion-title").not(this).removeClass("open");
+  $(".accordion-title").not(this).next().slideUp(300);
+  $(this).toggleClass("open");
+  $(this).next().slideToggle(300);
+});
 
 
- 
+//for side bar handling
+const sideBar_close = document.querySelector(".side_bar_close");
+const side_bar = document.querySelector("aside");
+const product_container = document.querySelector(".product_container");
+const sideBar_show = document.querySelector(".sidebar_show");
+
+sideBar_close.addEventListener("click", () => {
+  side_bar.style.display = "none";
+  product_container.style.width = "100%";
+  sideBar_show.style.display = "grid";
+
+})
+sideBar_show.addEventListener("click", () => {
+  side_bar.style.display = "block";
+  product_container.style.width = "calc(100% - 300px)";
+  sideBar_show.style.display="none"
+})
+//for sortby popup and filtaring popup
+const dropdown = document.querySelector(".pageShow_item_popup")
+const dropdown2 = document.querySelector(".short_list")
+const dropdown_btn = document.querySelector(".pageShow_select_btn")
+const dropdown_btn2 = document.querySelector(".short_by_btn")
+
+let isclicked = true;
+const dropdown_handler = (dropdown_area, dropdown_button) => {
+  dropdown_button.addEventListener("click", () => {
+    if (isclicked) {
+      dropdown_area.style.animation = "list_anim 1s linear forwards alternate";
+      dropdown_area.style.display = "block"
+    } else {
+      dropdown_area.style.animation = "list_anim_close 1s linear forwards alternate";
+      dropdown_area.style.display = "none"
+    }
+    isclicked = !isclicked;
+  })
+
+}
+dropdown_handler(dropdown, dropdown_btn)
+dropdown_handler(dropdown2, dropdown_btn2)
+
+
+
+
+
 //script for price range making
 const price_ranges = document.querySelectorAll(".range_div input");
 const progress_bar = document.querySelector(".price_progress");
